782001b3d849c285ae7f3985228d0b2df040829b
AKA你的闺蜜 <78334412+yourfriendyo@users.noreply.github.com>
2022-05-22 22:10:22 +0800

    C++IO流

---
+++ IOStream/IOStream/iostream.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+using namespace std;
+//int main()
+//{
+//	int i = 0;
+//	double d = 0.0;
+//
+//	cout << i;
+//	cout << d;
+//
+//	cin >> i;
+//	cin >> d;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	if (0) {
+//		string str;
+//		while (cin >> str) {
+//			cout << str << endl;
+//		}
+//	}
+//	if (1) {
+//		char a[128] = { 0 };
+//		while (scanf("%s", a) != EOF) {
+//			cout << a << endl;
+//		}
+//	}
+//	return 0;
+//}
+
+//class A {
+//public:
+//	A(int aa = 1)
+//		: _aa(aa)
+//	{}
+//	operator bool() 
+//	{
+//		return _aa;
+//	}
+////private:
+//	int _aa;
+//};
+//int main()
+//{
+//	A a;
+//	while (a) 
+//	{
+//		cin >> a._aa;
+//		cout << a._aa;
+//	}
+//	return 0;
+//}
+
