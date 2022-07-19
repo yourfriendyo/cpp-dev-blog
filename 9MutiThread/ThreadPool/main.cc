e2bc6acad2ffce4177f6191344d9bf6431038f30
yourfriendyo <3395537299@qq.com>
2022-07-19 22:48:35 +0800

    线程池

--- 9MutiThread/ThreadPool/main.cc
+++ 9MutiThread/ThreadPool/main.cc
@@ -1,3 +1,28 @@
 #include "ThreadPool.hpp"
+#include "Task.hpp"
+#include <ctime>
+#include <unistd.h>
 
-using namespace ThreadPool;
+using namespace NSThreadPool;
+using namespace NSTask;
+
+int main()
+{
+    srand((long long)time(nullptr));
+    ThreadPool<Task>* tp = new ThreadPool<Task>();
+    tp->InitThreadPool();
+
+    while (true)
+    {
+        Task t(rand() % 20 + 1, rand() % 10 + 1, "+-*/%"[rand() % 5]);
+
+        std::cout << "main thread make a task success: ";
+        t.ProducerPrintTask();
+        
+        tp->PushTask(t);
+
+        sleep(1);
+    }
+
+    return 0;
+}
