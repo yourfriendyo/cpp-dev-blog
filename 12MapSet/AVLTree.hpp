aff1546dda8e3d56094a8f16e75849b52e49b406
yourfriendyo <3395537299@qq.com>
2022-09-09 21:53:40 +0800

    avl

--- 12MapSet/AVLTree.hpp
+++ 12MapSet/AVLTree.hpp
@@ -1,118 +1,142 @@
 #pragma once
 #include <iostream>
 #include <cassert>
-
 using namespace std;
 
-template<class K, class V>
-struct AVLTNode 
+template <class K, class V>
+struct AVLTNode
 {
-    AVLTNode<K, V>*  _left;
+    AVLTNode<K, V>* _left;
     AVLTNode<K, V>* _right;
     AVLTNode<K, V>* _parent;
 
     pair<K, V> _kv;
-
     int _bf; // balance factor
 
-    AVLTNode(const pair<K, V> &kv)
-            : _left(nullptr), _right(nullptr), _parent(nullptr), _bf(0), _kv(kv)
+
+    AVLTNode(const pair<K, V>& kv)
+        :_left(nullptr), _right(nullptr), _parent(nullptr), _kv(kv), _bf(0)
     {}
 };
 
-template<class K, class V>
-class AVLTree 
+template <class K, class V>
+class AVLTree
 {
     typedef AVLTNode<K, V> Node;
 
 public:
-    AVLTree() : _root(nullptr) 
+    AVLTree() : _root(nullptr)
     {}
 
-    bool Insert(const pair<K, V> &kv) 
-    { if (_root == nullptr) 
+public:
+    bool Insert(const pair<K, V>& kv)
+    {
+        if (_root == nullptr)
         {
-            _root = new Node(kv);
-            return _root;
+            _root = new Node(kv); 
+            return true;
         }
 
-        Node *parent = nullptr;
-        Node *cur = _root;
-        while (cur) 
+        Node* parent = nullptr;
+        Node* curr = _root; 
+
+        while (curr)
         {
-            if (cur->_kv.first < kv.first) 
+            if (curr->_kv.first < kv.first)
             {
-                parent = cur;
-                cur = cur->_right;
-            } 
-            else if (cur->_kv.first > kv.first) 
+                parent = curr;
+                curr = curr->_right;
+            }
+            else if (curr->_kv.first > kv.first)
             {
-                parent = cur;
-                cur = cur->_left;
-            } 
+                parent = curr;
+                curr = curr->_left;
+            }
             else {
                 return false;
             }
         }
-        cur = new Node(kv);
-        if (parent->_kv.first < kv.first) 
+
+        curr = new Node(kv);
+
+        if (parent->_left == curr)       // 链接在左边
         {
-            parent->_right = cur;
-            cur->_parent = parent;
+            parent->_left = curr; 
+            curr->_parent = parent;
         }
-        else 
+        else if (parent->_right == curr) // 链接在右边
         {
-            parent->_left = cur;
-            cur->_parent = parent;
+            parent->_right = curr; 
+            curr->_parent = parent;
         }
 
         // 控制平衡
         // 1. 更新平衡因子
-        // 2. 旋转平衡处理异常平衡因子
-
-        while (parent) 
+        // 2. 旋转处理异常平衡因子
+        
+        while (parent) // 更新到根 
         {
-            if (cur == parent->_left) {
+            // 更新
+            if (curr == parent->_left)
+            {
                 parent->_bf--;
             }
-            else if (cur == parent->_right) {
+            else if (curr == parent->_right)
+            {
                 parent->_bf++;
             }
 
-            if (parent->_bf == 0) {
+            // 检测
+            if (parent->_bf == 0) // 已经平衡，更新结束
+            {
                 break;
             }
-            else if (parent->_bf == 1 || parent->_bf == -1) // 需要继续更新
+            else if (parent->_bf == 1 || parent->_bf == -1) // 向上更新
             {
-                cur = parent;
+                curr = parent;
                 parent = parent->_parent;
             }
-            else if (parent->_bf == 2 || parent->_bf == -2) // 进行旋转
+            else if (parent->_bf == 2 || parent->_bf == -2) // 平衡被打破，开始旋转
             {
-
-            } 
-            else {
-                assert(false);
+//                RotateR();
+//
+//                RotateR();
+//
+//                RotateRL();
+//
+//                RotateLR();
+            }
+            else // 树构建出错
+            {
+                assert(false); 
             }
         }
 
         return true;
     }
 
-private:
-    Node *_root;
+//    bool RotateR();
 
+//    bool RotateL();
+
+//    bool RotateRL();
+
+//    bool RotateLR();
+
+private:
+    Node* _root;
 };
 
 
-void TestAVLTree() 
+void TestAVLTree()
 {
-    AVLTree<int, int> t;
+    AVLTree<int, int>* avl = new AVLTree<int, int>();
 
-    int a[] = {1, 2, 3};
+    int a[] = { 1, 2, 3 };
 
-    for (auto e: a) 
+    for (auto e : a) 
     {
-        t.Insert(make_pair(e, e));
+        avl->Insert(make_pair(e, e));
     }
+
 }
