ee3dfdf50d1d6deea70115f971d4f76f17af2e35
yourfriendyo <3395537299@qq.com>
2022-09-21 00:18:58 +0800

    hash internal bone done

--- 13Hash/Hash.hpp
+++ 13Hash/Hash.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -245,34 +246,36 @@ namespace NS_Open_Hash
     {
         typedef HashNode<K, V> Node;
         HashFunc hf;
+
     public:
         Node* Find(const K& key)
         {
-            if (_table.size() == 0)
+            if (_table.empty())
                 return nullptr;
 
-            int pos = key % _table.size(); // 位置
-            Node* list = _table[pos];
+            size_t pos = hf(key) % _table.size(); // 位置
+            Node* curr = _table[pos];
 
-            while (list->_next)
+            while (curr)
             {
-                if (list->_kv.first == key) {
-                    return list;
+                if (curr->_kv.first == key) {
+                    return curr;
                 }
-                list = list->_next;
+                curr = curr->_next;
             }
+
             return nullptr;
         }
 
-        bool Insert(const pair<K, V> kv)
+        bool Insert(const pair<K, V>& kv)
         {
             if (Find(kv.first))
                 return false;
 
             // 扩容
-            if (_table.size() == 0 || _n == _table.size())
+            if (_table.empty() || _n == _table.size())
             {
-                size_t newSize = _table.size() == 0 ? 10 : _table.size() * 2;
+                size_t newSize = _table.empty() ? 10 : _table.size() * 2;
 
                 vector<Node*> newTable;
                 newTable.resize(newSize);
@@ -287,7 +290,7 @@ namespace NS_Open_Hash
                     {
                         next = curr->_next; // 记录下一个位置
 
-                        size_t pos = hf(curr->_kv.first) % _table.size(); // 找到插入位置
+                        size_t pos = hf(curr->_kv.first) % newSize; // 找到插入位置
                         // 头插
                         curr->_next = newTable[pos];
                         newTable[pos] = curr;
@@ -310,33 +313,46 @@ namespace NS_Open_Hash
             _table[pos] = newNode;
 
             ++_n;
+            return true;
         }
 
         bool Erase(const K& key)
         {
-            HashNode<K, V>* ret = Find(key);
+            Node* ret = Find(key);
             if (!ret)
                 return false;
 
             size_t pos = key % _table.size();
 
-            HashNode<K, V>* next = ret->_next;
-            HashNode<K, V>* prev = _table[pos]; // 链表起始位置
+            Node* prev = nullptr;
+            Node* curr = _table[pos]; // 链表起始位置
 
-            while (prev->_kv.first != key) {
-                prev = prev->_next;
+            while (curr)
+            {
+                if (curr->_kv.first == key)
+                {
+                    // 头删
+                    if (prev == nullptr) {
+                        _table[pos] = curr->_next;
+                    }
+                    else
+                    {
+                        prev->_next = curr->_next; // 链接
+                        delete ret; // 释放
+                    }
+                    --_n;
+                    return true;
+                }
+
+                prev = curr;
+                curr = curr->_next;
             }
 
-            prev->_next = next; // 链接
-            delete ret; // 释放
-
-            --_n;
-
-            return true;
+            return false;
         }
 
     private:
-        vector<HashNode<K, V>*> _table;
+        vector<Node*> _table;
         size_t _n;
 
     };
@@ -345,12 +361,29 @@ namespace NS_Open_Hash
     {
         HashTable<int, int> hash;
 
-        vector<int> v = { 4,24,14,7,37,27,57,67,34,14,54,64 };
+        vector<int> v = { 4,24,14,7,37,27,57,67,34,14,54 };
 
         for (auto e : v) {
             hash.Insert(make_pair(e, e));
         }
 
+        hash.Insert(make_pair(64, 64));
+
+
+        for (auto e : v)
+        {
+            cout << e << endl;
+
+            HashNode<int, int>* ret = hash.Find(e);
+            printf("%p\n", ret);
+
+            hash.Erase(e);
+
+            ret = hash.Find(e);
+            printf("%p\n", ret);
+
+            cout << "---------------------" << endl;
+        }
     }
 
 }
