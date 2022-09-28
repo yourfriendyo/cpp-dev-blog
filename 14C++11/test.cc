cd657c1efa5fcf9067f433a4a881e8aa270ba9e0
yourfriendyo <3395537299@qq.com>
2022-09-28 20:02:52 +0800

    test

--- 14C++11/test.cc
+++ 14C++11/test.cc
@@ -1,28 +1,47 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <list>
 using namespace std;
 
 struct Point
 {
-    int x;
-    int y;
+    int _x;
+    int _y;
+public:
+    Point(int x, int y) : _x(x), _y(y)
+    {}
+};
+
+class A
+{
+public:
+    A(int a) : _a(a)
+    {}
+private:
+    int _a;
 };
 
 int main()
 {
-    Point p = { 1 , 2 };             // C++98
-    Point pp{ 1 , 2 };               // C++11
-    Point parr[]{ {1, 2}, {3, 4} };  // C++11
+    Point p = { 1 , 2 };              // C++98
+    Point pp{ 1 , 2 };                // C++11
+    Point parr[]{ {1, 2}, {3, 4} };   // C++11
 
-    int a = 1;                       // C++98
-    int b = { 2 };                   // C++11
-    int b{ 2 };                      // C++11
-    int b({ 2 });                    // C++11
+    int a = 1;                        // C++98
+    int b1 = { 2 };                   // C++11
+    int b2{ 2 };                      // C++11
+    int b3({ 2 });                    // C++11
 
-    int* ptr = new int[5];           // C++98
-    int* ptr = new int[5] {1, 2, 3}; // C++11
+    int* ptr1 = new int[5];           // C++98
+    int* ptr2 = new int[5] {1, 2, 3}; // C++11
 
-    int arr[10] = { 1, 2, 3, 4 };    // C++98
-    vector<int> v = { 1,2, 3,4 };    // C++11
+    int arr[10] = { 1, 2, 3, 4 };     // C++98
+    vector<int> v = { 1,2, 3,4 };     // C++11
+
+    A a2 = 2;               // 单参数隐式类型转换
+    //Point pp{ 1 , 2 };    // 多参数隐式类型转换
+
+    vector<int> v1 = { 1,2,3,4,5,6 };
 
 };
