0e22bb20f824d10da85f829a8dfca6f8c17e8fc2
yourfriendyo <3395537299@qq.com>
2022-10-06 22:45:54 +0800

    sp

--- 16SmartPtr/Test.cpp
+++ 16SmartPtr/Test.cpp
@@ -2,9 +2,64 @@
 
 using namespace std;
 
+// int div()
+// {
+// 	int a = 0, b = 0;
+// 	cin >> a >> b;
+// 	if (b == 0)
+// 		throw invalid_argument("division by zero!");
+//
+// 	return a / b;
+// }
+//
+// void Func()
+// {
+// 	int* p = new int;
+// 	cout << div() << endl;
+//
+// 	cout << "delete p :" << p << endl;
+// 	delete p;
+// }
+//
+// int main()
+// {
+//     try
+//     {
+//         Func();
+//     }
+//     catch (const exception& e)
+//     {
+//         cout << e.what() << endl;
+//     }
+//
+//
+//     return 0;
+// }
+
+template <class T>
+class SmartPtr
+{
+public:
+    SmartPtr(T* ptr) : _ptr(ptr)
+    {}
+
+    ~SmartPtr()
+    {
+        delete _ptr;
+    }
+
+
+private:
+    T* _ptr;
+};
+
 int main()
 {
-	cout << "SmartPtr" << endl;
+    SmartPtr<int> sp1 = new int;
 
-	return 0;
+    int* p2 = new int;
+    SmartPtr<int> sp2 = p2;
+
+
+    return 0;
 }
