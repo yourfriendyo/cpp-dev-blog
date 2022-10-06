637abd2c4edd464331bce8bc448c568379a8845b
yourfriendyo <3395537299@qq.com>
2022-10-06 11:43:04 +0800

    st

--- 15Exception/Test.cpp
+++ 15Exception/Test.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Exception" << endl;
-
-	return 0;
-}
+// void Func()
+// {
+// 	int a, b;
+// 	cin >> a >> b;
+// 	if (b == 0)
+// 		throw "Division by zero condition";
+// 	else if (b == 1)
+// 		throw a;
+// 	else
+// 		cout << "result: " << a / b << endl;
+// }
+//
+// int main()
+// {
+// 	try {
+// 		Func();
+// 	}
+// 	catch (const char* errmsg) {
+// 		cerr << errmsg << endl;
+// 	}
+// 	catch (const int errnum) {
+// 		cerr << errnum << endl;
+// 	}
+// 	catch (...) {
+// 		cerr << "Unkown Exception" << endl;
+// 	}
+//
+// 	return 0;
+// }
