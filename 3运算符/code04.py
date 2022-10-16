709950f6cf0c1d4f9ed9544915ead50a28fdcd1f
yourfriendyo <3395537299@qq.com>
2022-10-17 02:14:01 +0800

    5

---
+++ 3运算符/code04.py
@@ -0,0 +1,68 @@
+result = (1 + 2) * 3
+print(result)
+
+a = 1
+# print(a / 0)   # ERR
+# print(a / 0.0) # ERR
+
+print(5 % 3)
+
+print(3 ** 2)
+
+# 地板除法 整数除法
+print(3 // 2)
+print(3 / 2)
+
+print(-3 // 2)
+print(-3 / 2)
+
+a = 1
+b = 0
+print(a > b)
+print(a < b)
+
+print(a >= b)
+print(a <= b)
+
+print(a != b)
+print(a == b)
+
+a = '你好'
+b = '世界'
+
+print(a > b)
+print(a < b)
+
+print(a >= b)
+print(a <= b)
+
+print(a != b)
+print(a == b)
+
+print(0.1)
+print(0.2)
+print(0.1 + 0.2 == 0.3)
+print(0.3)
+
+print(0.1)
+print(0.2)
+a = 0.1 + 0.2
+b = 0.3
+
+print(-0.00001 < (a - b) < 0.00001)
+
+a = 10
+b = 20
+c = 30
+
+print(a < b and b < c)
+print(c > b and b > a)
+
+# print(a < b < c)
+# print(c > b > a)
+
+print(a < b or b < c)
+print(c > b or b > a)
+
+print(not True)
+print(not False)
