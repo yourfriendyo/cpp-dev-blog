75b93b40f0116313949376bbfa9c7191766406d0
yourfriendyo <3395537299@qq.com>
2022-10-17 00:55:49 +0800

    5

---
+++ code01.py
@@ -0,0 +1,11 @@
+print(1 + 2 - 3)
+print(1 + 2 * 3)
+print(1 + 2 / 3)
+
+print((67.5 + 89.0 + 12.9 + 32.2) / 4)
+
+avg = (67.5 + 89.0 + 12.9 + 32.2) / 4
+total = (67.5 - avg) ** 2 + (89.0 - avg) ** 2 + (12.9 - avg) ** 2 + (32.2 - avg) ** 2
+result = total / 3
+
+print(result)
---
+++ code02.py
@@ -0,0 +1,51 @@
+a = 10
+
+# a * a = 10
+# if = 1
+
+a = 1
+b = a
+print(a)
+print(b)
+
+
+a = 1
+print(type(a))
+
+a = 1.1
+print(type(a))
+
+s = 'hello'
+print(type(s))
+
+s = "hello"
+print(type(s))
+
+e = "My name is \"yyx\""
+d = 'My name is "yyx"'
+f = "My name is 'yyx'"
+print(e)
+print(type(e))
+
+g = '''My 'name' is "yyx"'''
+h = """My "name" is 'yyx'"""
+
+print(len(d))
+print(len(f))
+print(len(g))
+print(len(h))
+
+a1 = "hello"
+a2 = 'world'
+print(a1 + " " + a2)
+
+# print(a1 + 1) # ERR
+
+a = True
+print(type(a))
+
+a: int = 1
+print(a)
+
+a: str = 'hello'
+print(a)
